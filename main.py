1d72d54d6f343aa956b4dd19222e962c0a2c4962
tunacom <tunacom@users.noreply.github.com>
2022-10-27 10:59:08 -0400

    Add additional debug logging

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import discord
+import logging
 import os
 
 from event_management import clean_event, update_event, update_role_for_user
+
+# TODO(tunacom): This can eventually be deleted.
+logging.basicConfig(filename='debug.log', encoding='utf-8', level=logging.DEBUG)
 
 TOKEN_PATH = os.path.join(os.path.dirname(__file__), 'discord_token')
 
@@ -10,12 +14,15 @@ TOKEN_PATH = os.path.join(os.path.dirname(__file__), 'discord_token')
 class MyClient(discord.Client):
 
     async def on_scheduled_event_create(self, event):
+        logging.debug('Event creation event: %s', event.id)
         await update_event(event)
 
     async def on_scheduled_event_delete(self, event):
+        logging.debug('Event deletion event: %s', event.id)
         await clean_event(event)
 
     async def on_scheduled_event_update(self, before, after):
+        logging.debug('Event update event: %s', after.id)
         await clean_event(before)
         if after.status in [
                 discord.EventStatus.scheduled, discord.EventStatus.active
@@ -23,9 +30,11 @@ class MyClient(discord.Client):
             await update_event(after)
 
     async def on_scheduled_event_user_add(self, event, user):
+        logging.debug('User add event: %s, %s', event.id, user.name)
         await update_role_for_user(event, user)
 
     async def on_scheduled_event_user_remove(self, event, user):
+        logging.debug('User remove event: %s, %s', event.id, user.name)
         await update_role_for_user(event, user, remove=True)
 
 
